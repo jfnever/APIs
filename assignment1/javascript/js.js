439c7d2b95823cb711f00e97371bccf6f61a1e3c
jfnever <jfnever@hotmail.com>
2016-04-14 13:17:03 -0500

    assignment1

---
+++ assignment1/javascript/js.js
@@ -0,0 +1,63 @@
+/**
+ * Created by apple on 16/4/14.
+ */
+var submitBtn = document.getElementById("submit-btn");
+var resetBtn = document.getElementById("reset-btn");
+
+submitBtn.addEventListener("click", onClickSubmit,false);
+resetBtn.addEventListener("click",onClickReset,false);
+
+var numberListAfter = document.getElementById("list");
+var countAfter = document.getElementById("count");
+var sumAfter = document.getElementById("sum");
+var averageAfter = document.getElementById("average");
+//var numberList = document.getElementById("list");
+var numberList = "Numbers:";
+var numberError = document.getElementById("error");
+
+var count = 0;
+var sum = 0;
+
+function onClickSubmit(evt){
+
+    evt.preventDefault();
+    numberError.innerHTML = "";
+
+    var input = document.getElementById('numbers').value;
+
+    if(isNaN(input)){
+
+        numberError.innerHTML = "Numbers only!";
+        window.alert("Numbers only! Please reset!");
+
+    }
+
+
+
+        numberList +=input.toString() + " ";
+        count++;
+        sum = sum + parseFloat(input);
+
+
+
+
+
+    numberListAfter.innerHTML = numberList;
+    countAfter.innerHTML = "Count:" + count;
+    sumAfter.innerHTML = "Sum:" + sum;
+    averageAfter.innerHTML = "Average:" + (sum/count);
+
+}
+
+function onClickReset(evt){
+
+    //numberList = "Numbers:";
+    count = 0;
+    numberListAfter.innerHTML = "Numbers:";
+    countAfter.innerHTML ="";
+    sumAfter.innerHTML = "";
+    averageAfter.innerHTML = "";
+    numberError.innerHTML = "";
+
+}
+
